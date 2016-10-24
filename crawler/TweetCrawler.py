dd593893e8cb916ccd6d09ef6c3d25aa9fb70aaa
Guilherme Peixoto <gpp@cin.ufpe.br>
2016-10-24 11:10:33 -0300

    feat(TweetCrawler.py): finishes crawler implementation

--- crawler/TweetCrawler.py
+++ crawler/TweetCrawler.py
@@ -1,31 +1,44 @@
 # coding=utf-8
-
 import sys
-import gzip
+import codecs
 import twitter
 import ujson as json
 import cPickle as pickle
+import time
+from datetime import datetime
+from random import choice
+from string import hexdigits
+
+POS = [':)', '(:', ':-)', '(-:']
+NEG = [':(', '):', ':-(', ')-:']
 
 class TweetCrawler:
     LANG = "lang"
     TEXT = "text"
     ENGLISH = "en"
-    SARCASM = "#sarcasm"
+    NEW_LINE = "\n"
 
-    def __init__(self, key_file_path, keywords=None):
+    def __init__(self, key_file_path, keywords=None, buffer_limit=100):
         self.key_file_path = key_file_path
         self.keywords = keywords
         self.stream = None
+        self.buffer = []
+        self.buffer_limit = buffer_limit
 
-    def __is_not_sarcasm__(self, text):
-        return text.find(TweetCrawler.SARCASM) == -1
+    def trim(self, text):
+        text = text.replace('\n', '').strip()
+        label = +1 if any(text.find(i) != -1 for i in POS) else -1
+        return json.dumps({"text": text, "label": label})
+
+    def __is_text_valid(self, text):
+        return text != None and text != "" and len(text) > 5
 
     def __is_tweet_valid__(self, tweet):
         if tweet.has_key(TweetCrawler.LANG) and tweet.has_key(TweetCrawler.TEXT):
             lang = tweet[TweetCrawler.LANG]
             text = tweet[TweetCrawler.TEXT]
             return lang == TweetCrawler.ENGLISH and \
-                self.__is_not_sarcasm__(text)
+                self.__is_text_valid(text)
 
         return False
 
@@ -42,11 +55,49 @@ class TweetCrawler:
         api = self.get_api_connection()
         self.stream = api.GetStreamFilter(track=self.keywords)
 
-    def crawl_tweets(self, keywords=None):
+    def __random_prefix__(self):
+        return ''.join([choice(hexdigits) for i in xrange(6)])
+
+    def is_buffer_full(self):
+        return len(self.buffer) >= self.buffer_limit
+
+    def get_file_name(self):
+        now = time.time()
+        dt = datetime.fromtimestamp(now)
+        ts = "%d%d%dT%d%d" % (dt.year, dt.month, dt.day, dt.hour, dt.minute)
+        fname = self.__random_prefix__() + "_" + ts + "_tweets.json"
+        return fname
+
+    def dump_buffer(self, path):
+        fname = path + self.get_file_name()
+        with codecs.open(fname, 'w', "utf-8") as f:
+            for tweet in self.buffer:
+                tweet = self.trim(tweet)
+                f.write(tweet + TweetCrawler.NEW_LINE)
+        self.buffer = []
+
+    def crawl_tweets(self, keywords=None, path='./data/'):
         if keywords is not None:
             self.keywords = keywords
         self.init_stream()
-        for tweet in self.stream:
-            if self.__is_tweet_valid__(tweet):
-                print tweet
-                break
+        for i, tweet in enumerate(self.stream):
+            print "Tweet: %d" % i
+            if self.__is_tweet_valid__(tweet) and not self.is_buffer_full():
+                self.buffer.append(tweet[TweetCrawler.TEXT])
+                print 'Valid on buffer: %d' % len(self.buffer)
+            elif self.is_buffer_full():
+                print "Dumping...\n...\n"
+                self.dump_buffer(path)
+            else:
+                continue
+
+def main():
+    keywords = POS + NEG
+    limit = sys.argv[-1]
+    crawler = TweetCrawler('./crawler/keys.txt',
+       keywords=keywords,
+       buffer_limit=1000)
+    crawler.crawl_tweets()
+
+if __name__ == '__main__':
+    main()
