463bc6ba9be9334b36f26bc552a97bd5e495270c
Guilherme Peixoto <gpp@cin.ufpe.br>
2016-10-29 12:10:35 -0300

    feat(train_w2v_model): reads tweet data and trains a word2vec model, stores on file

--- cnn/word2vec.py
+++ cnn/word2vec.py
@@ -1,11 +1,15 @@
-from gensim.models import word2vec as w2v
+from gensim.models.word2vec import Word2Vec
 from processor import process
+from random import choice
+from string import hexdigits
+from datetime import datetime
 import ujson as json
 import os
 
 TWEET_SUFFIX = "tweets.json"
 TEXT = "text"
 LABEL = "label"
+MODELS_DIR = "w2v_models"
 
 class TweetCorpusReader(object):
     """
@@ -31,5 +35,18 @@ class TweetCorpusReader(object):
         for json_file in json_files:
             with open(json_file, "r") as f_in:
                 tweets_in_file = [self.load_and_process(doc) for doc in f_in]
-                for tweet_label in tweets_in_file:
+                for tweet, label in tweets_in_file:
                     if len(tweet) > 1: yield (tweet, label)
+
+def train_w2v_model(corpus_path, model_path):
+    corpus = TweetCorpusReader(corpus_path)
+    model = Word2Vec(corpus, workers=4, window=3)
+    model.init_sims(replace=True)
+    if not os.path.exists(MODELS_DIR):
+        os.mkdir(MODELS_DIR)
+    rand_preffix = ''.join(choice(hexdigits) for i in xrange(6)).lower()
+    now = time.time()
+    dt = datetime.fromtimestamp(now)
+    ts = "%d%d%dT%d%d" % (dt.year, dt.month, dt.day, dt.hour, dt.minute)
+    model_name = rand_preffix + '_' + ts + ".model"
+    model.save('{0}/{1}'.format(model_path, model_name))
